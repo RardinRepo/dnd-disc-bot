6024ce7c398ba4ddc749ddf08a84315574492588
RardinRepo <58788134+RardinRepo@users.noreply.github.com>
2019-12-22 22:40:45 -0500

    discord bot start test

--- bot.js
+++ bot.js
@@ -1,42 +1,16 @@
 var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
-});
+const token = 'NjU4NDUyNDEyMDU0NjM0NTA3.XgAKSA.eHK7LL8RPv1wuzOYSXMiOBOAdyE';
+
+
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+    console.log('Bot is running');
+})
+
+client.login(token)
+
 
 // var charData = {
 //     name: '', race: '', subrace: '', dndclass: '', dndsubclass: '', level: 1, 
